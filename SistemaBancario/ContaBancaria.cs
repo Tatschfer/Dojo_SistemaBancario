164c8e93b62fd3306486f37fb64fbf0abd2e9b5b
Fernanda Tatsch <93793347+Tatschfer@users.noreply.github.com>
2026-01-20 23:07:30 -0300

    Add files via upload

---
+++ SistemaBancario/ContaBancaria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaBancario
+{
+    public class ContaBancaria
+    {
+        public int numero;
+        public string tipo;
+        public double saldo;
+
+        public ContaBancaria(int numero, string tipo, double saldo)
+        {
+            this.numero = numero;
+            this.tipo = tipo;
+            this.saldo = saldo;
+        }
+
+        public void ValidaConta(double saldo, string tipo)
+        {
+
+            if (tipo != "corrente" && tipo != "poupança")
+            {
+                throw new ArgumentException("Tipo de conta inválido.");
+            }
+
+        }
+
+        public void ValidaNumeroConta(List<int> contasAssociadas, int numeroConta)
+        {
+            if (contasAssociadas.Contains(numeroConta))
+            {
+                throw new ArgumentException("Número da conta inválido.");
+            }
+        }
+
+    }
+}
---
+++ SistemaBancario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco = new Banco();
+
+            banco.CadastrarCliente(12345678900, "Paulo Prates");
+            banco.CriarConta(12345, "corrente", 1000.0, 12345678900);
+            banco.ListarClientes();
+        }
+    }
+}
